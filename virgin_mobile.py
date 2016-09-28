41173f864cf4f05129d7a032f51719a2cec07586
Eric Siegel <eric.siegel@iremsolutions.com>
2016-09-28 16:55:37 -0400

    I tried to email myself an image file
    The MMS data that was received via email was different.
    There was no SMIL file and the content-type was
    application/vnd.wap.multipart.mixed
    
    Since it was an email, it contained a text/plain part and an image/png
    part (since I attached a PNG)
    
    To send MMS via email (for Virgin Mobile): 5555555555@vmpix.com

--- virgin_mobile.py
+++ virgin_mobile.py
@@ -88,7 +88,7 @@ class MMSMessage:
 		0x93: ("X-Mms-Response-Text"),
 		0x94: ("X-Mms-Sender-Visibility"),
 		0x95: ("X-Mms-Status"),
-		0x96: ("Subject"),
+		0x96: ("Subject", 'ascii'),
 		0x97: ("To", 'to'), # Note: There can be multiple "To" values
 		0x98: ("X-Mms-Transaction-Id", "ascii"),
 		0x99: ("X-Mms-Retrieve-Status", "boolean"),
@@ -378,7 +378,7 @@ class MMSMessage:
 				'contentLength': len(the_data),
 				'data': the_data
 			})
-		elif self.mms_content_type == 'application/vnd.wap.multipart.related':
+		elif self.mms_content_type.startswith('application/vnd.wap.multipart'):
 			# How many "parts" are in this "multipart" data?
 			parts = self.data[curr_index]
 			curr_index += 1
@@ -426,6 +426,7 @@ class MMSMessage:
 				# Read the next byte...
 				# 00-1E: Read that many bytes
 				# 1F: Next byte is length
+				# 80-FF: This byte is the data
 				# This range contains the content-type and its charset
 				if 0x00 <= data_header[data_header_index] <= 0x1E:
 					content_type_length = data_header[data_header_index]
@@ -433,9 +434,16 @@ class MMSMessage:
 				elif data_header[data_header_index] == 0x1F:
 					content_type_length = data_header[data_header_index+1]
 					data_header_index += 2
+				elif 0x80 <= data_header[data_header_index] <= 0xFF:
+					# This byte *is* the data
+					# Don't shift data_header_index, just re-read this byte
+					content_type_length = 1
 
 				content_type_range = data_header[data_header_index:data_header_index+content_type_length]
 				data_header_index += content_type_length
+
+				# A single byte will be read as an int, convert it back to a bytes object
+				content_type_range = bytes([content_type_range]) if type(content_type_range) is int else content_type_range
 
 				# Get the content type
 				# How should we intrepret this?
@@ -459,13 +467,13 @@ class MMSMessage:
 					# Look it up in the MIME type table
 					data_content_type = self.mime_types[content_type_range[0]]
 
-					# What charset is being used?
-					data_charset = self.charsets[content_type_range[2] if content_type_range[1] == 0x81 else content_type_range[1]]
+					if len(content_type_range) >1:
+						# What charset is being used, if any?
+						data_charset = self.charsets[content_type_range[2] if content_type_range[1] == 0x81 else content_type_range[1]]
 
-					# There is more data included after.  0x85 seems to be the "file name", again
-					data_content_extra = (content_type_range[3:] if content_type_range[1] == 0x81 else content_type_range[2:]).lstrip(b'\x85').rstrip(b'\x00').decode('utf_8')
+						# There is more data included after.  0x85 seems to be the "file name", again
+						data_content_extra = (content_type_range[3:] if content_type_range[1] == 0x81 else content_type_range[2:]).lstrip(b'\x85').rstrip(b'\x00').decode('utf_8')
 
-				# The next byte should be 0xC0
 				# Followed by the "Content-ID" (this may not match the one from earlier)
 				# This is just the rest of the remaining bytes before the data
 				# I don't actually know what it is or how to decode it
@@ -473,11 +481,22 @@ class MMSMessage:
 				# We've read the "content-type length" (1 byte) and the "content-type"
 				data_content_id = data_header[data_header_index:]
 
-				# Split this into multiple parts
-				# The 1st seems to be a consistent 0xC0 0x22
-				# With the file name inside `<>`
-				data_content_id = data_content_id.rstrip(b'\x00').split(b'\x00')
-				file_name = data_content_id[0].lstrip(b'\xc0\x22').decode('utf_8')[1:-1]
+				# If we have 0xC0, then we can decode the file name from there
+				if data_content_id.startswith(b'\xc0\x22'):
+					# Split this into multiple parts
+					# The 1st seems to be a consistent 0xC0 0x22
+					# With the file name inside `<>`
+					data_content_id = data_content_id.rstrip(b'\x00').split(b'\x00')
+					file_name = data_content_id[0].lstrip(b'\xc0\x22').decode('utf_8')[1:-1]
+				elif len(data_content_id) > 0:
+					# We have some other type of "Content-ID" data
+					# When I emailed myself an image, this contained the file name
+					# After an 0x86 byte (before that was 0xAE 0x0F 0X81)
+					file_name_index = data_content_id.find(b'\x86')
+					file_name = data_content_id[file_name_index+1:-1].decode('utf_8') if file_name_index >= 0 else ''
+				else:
+					# "Content-ID" is blank
+					file_name = ''
 
 				# Ok, we're done with the content headers.
 				# We know the length of the data, let's read that many bytes!
@@ -545,7 +564,7 @@ if __name__ == '__main__':
 		if mms_headers['Content-Type'] == 'text/plain':
 			# MMS message contains an error message
 			print('MMS Error:', mms_data[0]['data'])
-		elif mms_headers['Content-Type'] == 'application/vnd.wap.multipart.related':
+		elif mms_headers['Content-Type'].startswith('application/vnd.wap.multipart'):
 			# Print out some of the more important headers
 			print("From:\n\t", mms_headers['From'])
 			print("To:\n\t", mms_headers['To'])
@@ -556,8 +575,11 @@ if __name__ == '__main__':
 			for file_data in mms_data:
 				# We have an image.  Should we extract it?
 				if file_data['contentType'].startswith('image/') and args.extract:
-					# TODO: This may not be a JPEG file, but I'm pretty sure it always is.
-					file_data['data'].save(file_data['fileName'], 'jpeg', exif=file_data['data'].info["exif"])
+					# Only JPEGs can have EXIFs (most cell phones will add this when texting an image)
+					if file_data['contentType'] == ' image/jpeg':
+						file_data['data'].save(file_data['fileName'], 'jpeg', exif=file_data['data'].info["exif"])
+					else:
+						file_data['data'].save(file_data['fileName'])
 				# This is just a text, display it
 				elif file_data['contentType'] == 'text/plain':
-					print("\n", file_data['data'])
+					print("Text:\n\t", file_data['data'])
